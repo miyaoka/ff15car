d47a033544d91eb8f31ee36038817059bef1c9af
Masaya Kazama <miyan@t-p.jp>
2014-10-03 05:39:46 +0900

    マウス位置に応じて画像が被らないように引っ込むようにした

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var animTime = 300;
+var imgMargin = 100;
 var imgs = [
   'FF15_A_fix.png',
   'FF15_B_fix.png',
@@ -7,12 +9,35 @@ var imgs = [
 ];
 var imgSrc = chrome.extension.getURL(
   [
-    'images', 'footer', imgs[Math.floor(Math.random()*imgs.length)]
+  'images', 'footer', imgs[Math.floor(Math.random()*imgs.length)]
   ].join('/')
 );
 
-var img = document.createElement('img');
-img.src = imgSrc;
-img.id = 'ff15-car-footer';
+var img = $('<img>').attr('src', imgSrc).attr('id', 'ff15-car-footer');
 
-document.body.appendChild(img);
+var lastDest = null;
+function scroll(dest){
+  if(lastDest == dest){
+    return;
+  }
+  lastDest = dest;
+  img.stop();
+  img.animate({
+    'bottom': dest + 'px'
+  }, animTime);
+}
+$(document).ready(function () {
+  $('body').append(img);
+  img.css('bottom', -img.height() * .5);
+  $('html').mousemove(function(e){
+    var wh = window.innerHeight ? window.innerHeight: $(window).height();
+    var imgTop = wh - img.height() - imgMargin;
+    var diff = e.clientY - imgTop;
+    if(diff > 0){
+      scroll(-diff);
+    } else {
+      scroll(0);
+    }
+  });
+  scroll(0);
+});
